6d79f7ba619249f544c4cb90907c033429aff576
XuChao19950804 <1390420504@qq.com>
2020-07-26 01:36:33 +0800

    111

--- src/main/java/com/jenkins/test/JenkinsController.java
+++ src/main/java/com/jenkins/test/JenkinsController.java
@@ -18,6 +18,6 @@ public class JenkinsController {
 
     @RequestMapping(value = "/guanguan")
     public String woman(){
-        return "adff asdf sssbc";
+        return "adff asdf sss111bc";
     }
 }
